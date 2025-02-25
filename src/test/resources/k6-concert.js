81a5fe0436d3525dc3b95b08b0de110c1173d6a2
Tareun3406 <tareun3406@gmail.com>
2025-02-25 17:08:19 +0900

    test: k6 스크립트 추가

---
+++ src/test/resources/k6-concert.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+    vus: 100,
+    duration: '1m'
+}
+
+export default function () {
+    const api = "http://hhconcert.tareun.kr"
+
+    const moveMaxPage = Math.floor(Math.random() * 10) // 몇번째 페이지 까지 조회할지 랜덤 선택
+    let concertList;
+    for (let i = 0; i <= moveMaxPage; i ++) {
+        concertList = http.get(`${api}/concerts?pageNumber=${i}`);
+        sleep(5)
+    }
+}
